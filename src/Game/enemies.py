a50ecf9affad60cef556534b027f5a89d14b7d64
Theunis-Moolman <28904516@sun.ac.za>
2026-03-31 12:42:18 +0200

    Converted enemies and spaceship to class objects (Logic was kept the same)

--- src/Game/enemies.py
+++ src/Game/enemies.py
@@ -1,58 +1,69 @@
-def is_enemy_hit_by_laser(enemy, laser_origin, laser_direction, enemy_radius):
-    #Enemy position
-    ex  = enemy['x']
-    ey  = enemy['y']
+import math
+import stddraw
 
-    #Laser start + direction
-    lx, ly = laser_origin
-    dx, dy = laser_direction
+class Enemy:
+    def __init__(self, x, y, colour):
+        self.enemy_x = x
+        self.enemy_y = y
+        self.colour = colour
 
-    #playsound("Explode")
+    def is_enemy_hit_by_laser(self, laser_origin, laser_direction, enemy_radius):
+        #Laser start + direction
+        lx, ly = laser_origin
+        dx, dy = laser_direction
 
-    #Distance from line formula                
-    numerator = abs((ey - ly) * dx - (ex - lx) * dy)
-    denominator = math.hypot(dx, dy)
-    distance = numerator / denominator
-    return distance <= enemy_radius #Hit if close enough
+        #playsound("Explode")
 
-def create_enemies(level):
-    enemies = []
-    enemy_r = 5
-    enemy_rows = 2
-    enemy_col  = 5
-    enemy_spacing = 15
-    enemy_x = 15
-    enemy_y = 90
+        #Distance from line formula
+        numerator = abs((self.enemy_y - ly) * dx - (self.enemy_y - lx) * dy)
+        denominator = math.hypot(dx, dy)
+        distance = numerator / denominator
+        return distance <= enemy_radius #Hit if close enough
 
-    #Create grid
-    for row in range(enemy_rows):
-        for col in range(enemy_col):
-            x_pos = enemy_x + col * enemy_spacing
-            y_pos = enemy_y - row * enemy_spacing
-            #Level-based coloring
-            color = stddraw.BLUE if level == 1 else stddraw.RED if row == 1 else stddraw.BLUE
-            enemies.append({'x': x_pos,'y':y_pos, 'color': color})
-    return enemies
 
-def enemy_update(enemies, enemy_dir,enemy_speed, enemy_radius):
-    should_decend = False
+class Enemies:
+    def __init__(self, rows, cols):
+        self.enemies = []
+        self.enemy_spacing = 15
+        self.enemy_rows = rows # 2
+        self.enemy_cols = cols # 5
 
-    for enemy in enemies:
-        next_x = enemy['x'] + enemy_dir * enemy_speed
-        #Check wall collision
-        if next_x + enemy_radius > 100 or next_x - enemy_radius < 0:
-            should_decend = True
-            break
-    
-    for enemy in enemies:
-        #Move enemies
-        if should_decend:
-            enemy['y'] -= 5
-        else:
-            enemy['x'] += enemy_dir * enemy_speed
-            
-    #Reverse direction
-    if should_decend:
-        enemy_dir *= -1
+    def create_enemies(self, level):
+        enemy_x = 15
+        enemy_y = 90
 
-    return enemies, enemy_dir
+        #Create grid
+        for row in range(self.enemy_rows):
+            for col in range(self.enemy_cols):
+                x_pos = enemy_x + col * self.enemy_spacing
+                y_pos = enemy_y - row * self.enemy_spacing
+
+                #Level-based coloring
+                colour = stddraw.BLUE if level == 1 else stddraw.RED if row == 1 else stddraw.BLUE
+
+                new_enemy = Enemy(x_pos, y_pos, colour)
+
+                self.enemies.append(new_enemy)
+
+    def enemy_update(self, enemy_dir, enemy_speed, enemy_radius):
+        should_descend = False
+
+        for enemy in self.enemies:
+            next_x = enemy.enemy_x + enemy_dir * enemy_speed
+            #Check wall collision
+            if next_x + enemy_radius > 100 or next_x - enemy_radius < 0:
+                should_descend = True
+                break
+
+        for enemy in self.enemies:
+            #Move enemies
+            if should_descend:
+                enemy.enemy_y -= 5
+            else:
+                enemy.enemy_x += enemy_dir * enemy_speed
+
+        #Reverse direction
+        if should_descend:
+            enemy_dir *= -1
+
+        return enemy_dir
--- src/Game/spaceship.py
+++ src/Game/spaceship.py
@@ -1,103 +1,113 @@
 import stddraw # type: ignore
 import math
-from music import Music
+from Music import music
 
-#Move player horizontally
-def move_circle(x,radius,direction,speed):
-    if x - radius < 0: #Prevents going off left edge
-        x = radius
-    if x + radius > 100: #Prevents going off right edge
-        x = 100 - radius
-    x += direction * speed
-    return x #Returns updatd position
 
-#Controls aiming direction
-def line_rotate(angle , clockwise , counterclockwise , rotation_speed):
-    #Rotate left
-    if counterclockwise:
-        angle += rotation_speed #increase angle
-        if angle >= 180: #Clamp at 180 degrees cannot go further
-            angle = 180
-            counterclockwise = False
-    
-    #Rotate right
-    if clockwise:
-        angle -= rotation_speed #Decrease angle
-        if angle <= 0:
-            angle = 0
-            clockwise = True
+class Player:
+    def __init__(self, x, y, radius, direction, speed, angle):
+        self.x  = x
+        self.y = y
+        self.radius = radius
+        self.direction = direction
+        self.speed = speed
+        self.angle = angle
+        self.dx = 0
+        self.dy = 0
+        self.pixels = []
 
-    return angle , counterclockwise , clockwise  #Returns updated values
+    #Move player horizontally
+    def move_circle(self, direction,speed):
+        if self.x - self.radius < 0: #Prevents going off left edge
+            self.x = self.radius
+        if self.x + self.radius > 100: #Prevents going off right edge
+            self.x = 100 - self.radius
+        self.x += direction * speed
 
-def pixel(px, py, color):
-    size=0.5
-    stddraw.setPenColor(color)
-    stddraw.filledSquare(px, py, size)
 
-def draw_spaceship(x, y, radius, angle, line_length,change_color,is_hit=False):
-    size = 0.5  # Size of each pixel/block
-    hit_color = change_color if is_hit else stddraw.WHITE  #If hit, flash color
+    #Controls aiming direction
+    def line_rotate(self, is_clockwise , counterclockwise , rotation_speed):
+        #Rotate left
+        if counterclockwise:
+            self.angle += rotation_speed #increase angle
+            if self.angle >= 180: #Clamp at 180 degrees cannot go further
+                self.angle = 180
+                counterclockwise = False
 
-    # Define the spaceship shape using relative coordinates (dx, dy, color)
-    pixels = [] #List of ship parts
+        #Rotate right
+        if is_clockwise:
+            self.angle -= rotation_speed #Decrease angle
+            if self.angle <= 0:
+                self.angle = 0
+                is_clockwise = True
 
-    # Tip of ship
-    pixels.append((0, 10, hit_color))
+        return counterclockwise, is_clockwise  #Returns updated values
 
-    # wings and body using for loops
-    for i in [-1, 1]:
-        pixels.append((i, 9, hit_color))
-        pixels.append((2 * i, 8, hit_color))
-        pixels.append((3 * i, 7, hit_color))
-        pixels.append((3 * i, 6, hit_color))
-        pixels.append((3 * i, 5, hit_color))
-        pixels.append((4 * i, 4, hit_color))
-        pixels.append((5 * i, 3, hit_color))
-        pixels.append((6 * i, 2, hit_color))
+    def pixel(self, px, py, color):
+        size=0.5
+        stddraw.setPenColor(color)
+        stddraw.filledSquare(px, py, size)
 
-        for j in range(-1, -4, -1):
-            pixels.append((7 * i, j, hit_color))
+    def draw_spaceship(self, line_length,change_color,is_hit=False):
+        size = 0.5  # Size of each pixel/block
+        hit_color = change_color if is_hit else stddraw.WHITE  #If hit, flash color
 
-        pixels.append((7 * i, 0, hit_color))
-        pixels.append((7 * i, 1, hit_color))
-        pixels.append((6 * i, -4, hit_color))
-        pixels.append((5 * i, -4, hit_color))
-        pixels.append((4 * i, -3, hit_color))
-        pixels.append((3 * i, -3, hit_color))
-        pixels.append((2 * i, -3, hit_color))
-        pixels.append((1 * i, -3, hit_color))
+        # Define the spaceship shape using relative coordinates (dx, dy, color)
+        self.pixels = []
+        # Tip of ship
+        self.pixels.append((0, 10, hit_color))
 
-        # Flame in red
-        for k in range(1, 5):
-            pixels.append((k * i, -2, stddraw.RED if not is_hit else hit_color))
+        # wings and body using for loops
+        for i in [-1, 1]:
+            self.pixels.append((i, 9, hit_color))
+            self.pixels.append((2 * i, 8, hit_color))
+            self.pixels.append((3 * i, 7, hit_color))
+            self.pixels.append((3 * i, 6, hit_color))
+            self.pixels.append((3 * i, 5, hit_color))
+            self.pixels.append((4 * i, 4, hit_color))
+            self.pixels.append((5 * i, 3, hit_color))
+            self.pixels.append((6 * i, 2, hit_color))
 
-        # Vertical midsection
-        for k in range(-1, 4):
-            pixels.append((2 * i, k, hit_color))
-    
-    # Convert angle to radians for math functions
-    radian = math.radians(angle)-math.pi/2
-    radians = math.radians(angle)
+            for j in range(-1, -4, -1):
+                self.pixels.append((7 * i, j, hit_color))
 
-    # Draw each part of the ship after rotation(Relative to ship position)
-    for dx, dy, color in pixels:
-        pixel(x + dx * size, y + dy * size, color)
+            self.pixels.append((7 * i, 0, hit_color))
+            self.pixels.append((7 * i, 1, hit_color))
+            self.pixels.append((6 * i, -4, hit_color))
+            self.pixels.append((5 * i, -4, hit_color))
+            self.pixels.append((4 * i, -3, hit_color))
+            self.pixels.append((3 * i, -3, hit_color))
+            self.pixels.append((2 * i, -3, hit_color))
+            self.pixels.append((1 * i, -3, hit_color))
 
-    # Optional: Draw a direction line
-    end_x = x + line_length * math.cos(radians)
-    end_y = y + line_length * math.sin(radians)
-    stddraw.setPenColor(stddraw.RED)
-    stddraw.line(x, y, end_x, end_y) #Shows where you are aiming 
+            # Flame in red
+            for k in range(1, 5):
+                self.pixels.append((k * i, -2, stddraw.RED if not is_hit else hit_color))
 
-def shoot(x,y,angle,speed_projectile):
-    radians = math.radians(angle) #Convert angles
-    
-    #Calculate movement direction
-    dx = speed_projectile * math.cos(radians)
-    dy = speed_projectile * math.sin(radians)
+            # Vertical midsection
+            for k in range(-1, 4):
+                self.pixels.append((2 * i, k, hit_color))
 
-    Music.sound(1000,44100) #Play shooting sound
-    
-    
+        # Convert angle to radians for math functions
+        #radian = math.radians(angle)-math.pi/2
+        radians = math.radians(self.angle)
 
-    return {'x':x , 'y':y , 'dx':dx , 'dy':dy}
+        # Draw each part of the ship after rotation(Relative to ship position)
+        for dx, dy, color in self.pixels:
+            self.pixel(self.x + dx * size, self.y + dy * size, color)
+
+        # Optional: Draw a direction line
+        end_x = self.x + line_length * math.cos(radians)
+        end_y = self.y + line_length * math.sin(radians)
+        stddraw.setPenColor(stddraw.RED)
+        stddraw.line(self.x, self.y, end_x, end_y) #Shows where you are aiming
+
+    def shoot(self, speed_projectile):
+        radians = math.radians(self.angle) #Convert angles
+
+        #Calculate movement direction
+        dx = speed_projectile * math.cos(radians)
+        dy = speed_projectile * math.sin(radians)
+
+        music.sound(1000,44100) #Play shooting sound
+
+        return dx, dy #use Player.x and Player.y to get x and y
