46c955fa78f6d628a54ac63734cd857dc95eb38f
Theunis-Moolman <28904516@sun.ac.za>
2026-04-01 13:07:22 +0200

    Removal of venv folder and added a requirements.txt file for easy setup. - To set up use "pip install -r requirements.txt"

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from src.states.menu import MenuPage
 from src.states.game import GamePlay
 
 def main() -> None:
-    width = 800
-    height = 800
+    width = 600
+    height = 600
     Menu = MenuPage(width, height)
     state = "MENU"
 
