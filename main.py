233b80e4166c2be4b626ff89702d5cdd62ac498c
Theunis-Moolman <28904516@sun.ac.za>
2026-04-06 16:44:09 +0200

    FIX

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main() -> None:
                 transitioned = True
                 GamePage = Level1(width, height)
             else:
-                if GamePage.score >= 15000 and not isinstance(GamePage, Level2):
+                if 15000 <= GamePage.score < 40000 and not isinstance(GamePage, Level2):
                     Transition = TransitionPage("Level 2", level2_paragraph, GamePage.stars)
                     Transition.draw()
                     GamePage = Level2(width, height, GamePage.stars)
